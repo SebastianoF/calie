cd3bdff93303e39fd8e89bc7147cdd963dfed19f
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2018-12-24 18:24:42 +0100

    Bench

--- benchmarking/b_path_manager.py
+++ benchmarking/b_path_manager.py
@@ -1,3 +1,10 @@
 import os
 
-root_dir = os.path.abspath(os.path.dirname(__file__))
+root_dir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+data_adni_longitudinal = ''
+data_adni_brainweb = ''
+
+data_output_dir = '/Users/sebastiano/a_data/TData/A4_bench'
+
+assert os.path.exists(data_output_dir)
---
+++ benchmarking/bm0_slice_resampling.py
@@ -0,0 +1,16 @@
+import os
+import numpy as np
+
+
+def run_experiment(control, param):
+    pass
+
+
+if __name__ == '__main__':
+    controller = {'prepare_data' : True,
+                  'elaborate'    : True,
+                  'save_results' : True,
+                  'show_results' : True}
+    parameters = {}
+
+    run_experiment(controller, parameters)
