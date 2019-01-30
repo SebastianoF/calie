76b2b02678b5e9800ef04b2b1022d8badce62e49
Sebastiano Ferraris <sebastiano.ferraris@gmail.com>
2019-01-30 20:08:31 +0000

    Building benchmarking in progress

--- benchmarking/b_path_manager.py
+++ benchmarking/b_path_manager.py
@@ -9,6 +9,9 @@ root_data = '/Users/sebastiano/a_data/TData/'
 pfo_adni = jph(root_data, 'ADNI_longitudinal')
 pfo_brainweb = jph(root_data, 'BrainWeb')
 
+assert os.path.exists(pfo_adni)
+assert os.path.exists(pfo_brainweb)
+
 pfo_output_A1     = jph(root_data, 'A1_bench')
 pfo_output_A1_3d  = jph(root_data, 'A1_bench_3d')
 pfo_output_A4_SE2 = jph(root_data, 'A4_bench_SE2')
@@ -17,9 +20,7 @@ pfo_output_A4_GL2 = jph(root_data, 'A4_bench_GL2')
 pfo_output_A4_GAU = jph(root_data, 'A4_bench_GAU')
 pfo_output_A4_BW  = jph(root_data, 'A4_bench_BW')
 pfo_output_A4_AD  = jph(root_data, 'A4_bench_AD')
-
-assert os.path.exists(pfo_adni)
-assert os.path.exists(pfo_brainweb)
+pfo_output_A5_3T  = jph(root_data, 'A5_IC_SA_SE')
 
 assert os.path.exists(pfo_output_A1)
 assert os.path.exists(pfo_output_A1_3d)
@@ -28,3 +29,4 @@ assert os.path.exists(pfo_output_A4_GL2)
 assert os.path.exists(pfo_output_A4_GAU)
 assert os.path.exists(pfo_output_A4_BW)
 assert os.path.exists(pfo_output_A4_AD)
+assert os.path.exists(pfo_output_A5_3T)
--- benchmarking/bm7_3_assessments.py
+++ benchmarking/bm7_3_assessments.py
@@ -23,12 +23,13 @@ from nilabels.tools.aux_methods.utils import print_and_run
 from nilabels.tools.aux_methods.utils_nib import set_new_data
 
 from calie.fields import queries as qr
+from calie.fields import compose as cp
 from calie.fields import coordinate as coord
 
-from benchmarking.a_main_controller import methods, spline_interpolation_order, steps, num_samples, bw_subjects, \
+from benchmarking.a_main_controller import methods, spline_interpolation_order, num_samples, bw_subjects, \
     ad_subjects
 from benchmarking.b_path_manager import pfo_output_A4_SE2, pfo_output_A4_GL2, pfo_output_A4_HOM, \
-    pfo_output_A4_GAU, pfo_output_A4_BW, pfo_output_A4_AD
+    pfo_output_A4_GAU, pfo_output_A4_BW, pfo_output_A4_AD, pfo_output_A5_3T
 
 
 def three_assessments_collector(control, verbose):
@@ -60,16 +61,155 @@ def three_assessments_collector(control, verbose):
     for pfi in pfi_svf_list:
         assert os.path.exists(pfi)
 
+    # --------------------------------------- #
+    # Select number of steps for each method
+    # --------------------------------------- #
+
+    if control['computation'] == 'IC':
+        steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30]
+    elif control['computation'] == 'SA':
+        steps = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 30]
+    elif control['computation'] == 'SE':
+        steps = range(1, 30)
+
+    else:
+        raise IOError('Input control computation {} not defined.'.format(control['computation']))
+
     if control['collect']:
 
         print('---------------------------------------------------------------------------')
-        print('Collecting {} dataset  : bw-<s>-<algebra/group>.npy sj in BrainWeb '.format('computation'))
+        print('Test {} for dataset {} '.format(control['computation'], control['svf_dataset']))
         print('---------------------------------------------------------------------------')
 
+        for pfi_svf in pfi_svf_list:
+            sj_id = os.path.basename(pfi_svf).split('-')[:2]
+            sj_id = sj_id[0] + '-' + sj_id[1]
+
+            method_names = [k for k in methods.keys() if methods[k][1]]
+
+            df_steps_measures = pd.DataFrame(columns=method_names, index=steps)
+            svf1 = np.load(pfi_svf)
+
+            for met in method_names:
+                print('Computing method {}.'.format(met))
+                exp_method = methods[met][0]
+
+                for st in steps:
+
+                    if control['computation'] == 'IC':
+                        exp_st_svf1     = exp_method(svf1, input_num_steps=st)
+                        exp_st_neg_svf1 = exp_method(-1 * svf1, input_num_steps=st)
+                        error = 0.5 * (qr.norm(cp.lagrangian_dot_lagrangian(exp_st_svf1, exp_st_neg_svf1)) +
+                                       qr.norm(cp.lagrangian_dot_lagrangian(exp_st_neg_svf1, exp_st_svf1)))
+
+                    elif control['computation'] == 'SA':
+                        a, b, c = 0.3, 0.3, 0.4
+                        exp_st_a_svf1 = exp_method(a * svf1, input_num_steps=st)
+                        exp_st_b_svf1 = exp_method(b * svf1, input_num_steps=st)
+                        exp_st_c_svf1 = exp_method(c * svf1, input_num_steps=st)
+                        error = qr.norm(cp.lagrangian_dot_lagrangian(cp.lagrangian_dot_lagrangian(exp_st_a_svf1, exp_st_b_svf1), exp_st_c_svf1))
+
+                    elif control['computation'] == 'SE':
+                        exp_st_svf1          = exp_method(svf1, input_num_steps=st)
+                        exp_st_plus_one_svf1 = exp_method(svf1, input_num_steps=st+1)
+                        error = qr.norm(exp_st_svf1 - exp_st_plus_one_svf1)
+
+                    else:
+                        raise IOError('Input control computation {} not defined.'.format(control['computation']))
+
+                    df_steps_measures['measure'][st] = error
+
+                print(df_steps_measures)
+
+            fin_output = 'test_{}_{}.csv'.format(control['computation'], sj_id)
+            df_steps_measures.to_csv(jph(pfo_output_A5_3T, fin_output))
+            print('Test result saved in:')
+            print(fin_output)
+            print('\n')
+
+    else:
+        # assert pandas data-frame exists.
+        for pfi_svf in pfi_svf_list:
+            sj_id = os.path.basename(pfi_svf).split('-')[:2]
+            sj_id = sj_id[0] + '-' + sj_id[1]
+            fin_output = 'test_{}_{}.csv'.format(control['computation'], sj_id)
+            assert os.path.exists(jph(pfo_output_A5_3T, fin_output)), jph(pfo_output_A5_3T, fin_output)
+
+    ##################
+    # get statistics #
+    ##################
+
+    if control['get_statistics']:
+
+        print('------------------------------------------------')
+        print('\n Statistics for computation {} '.format(control['computation']))
+
+        method_names = [k for k in methods.keys() if methods[k][1]]
+
+        df_means_methods = pd.DataFrame(columns=method_names, index=steps)
+        df_stds_methods  = pd.DataFrame(columns=method_names, index=steps)
+
+        for pfi_svf in pfi_svf_list:
+            sj_id = os.path.basename(pfi_svf).split('-')[:2]
+            sj_id = sj_id[0] + '-' + sj_id[1]
+
+
+
         pass
 
-    if control['show']:
-        pass
+    ###############
+    # show graphs #
+    ###############
+
+    if control['show_graphs']:
+
+        font_top = {'family': 'serif', 'color': 'darkblue', 'weight': 'normal', 'size': 14}
+        font_bl = {'family': 'serif', 'color': 'black', 'weight': 'normal', 'size': 12}
+        legend_prop = {'size': 12}
+
+        sns.set_style()
+
+        fig, ax = plt.subplots(figsize=(7, 7))
+
+        fig.canvas.set_window_title('{}_{}.pdf'.format(control['svf_dataset'], control['computation']))
+
+        for method_name in [k for k in methods.keys() if methods[k][1]]:
+
+            pfi_df_mean_std = jph(pfo_output_A4_SE2, 'se2-stats-{}.csv'.format(method_name))
+            df_mean_std = pd.read_csv(pfi_df_mean_std)
+
+            ax.plot(df_mean_std['mu_time'].values,
+                    df_mean_std['mu_error'].values,
+                    label=method_name,
+                    color=methods[method_name][3],
+                    linestyle=methods[method_name][4],
+                    marker=methods[method_name][5])
+
+            for i in df_mean_std.index:
+                el = mpatches.Ellipse((df_mean_std['mu_time'][i], df_mean_std['mu_error'][i]),
+                                      df_mean_std['std_time'][i], df_mean_std['std_error'][i],
+                                      angle=0,
+                                      alpha=0.2,
+                                      color=methods[method_name][3],
+                                      linewidth=None)
+                ax.add_artist(el)
+
+        ax.set_title('Time error for SE(2)', fontdict=font_top)
+        ax.legend(loc='upper right', shadow=True, prop=legend_prop)
+
+        ax.xaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
+        ax.yaxis.grid(True, linestyle='-', which='major', color='lightgrey', alpha=0.5)
+        ax.set_axisbelow(True)
+
+        ax.set_xlabel('Time (sec)', fontdict=font_bl, labelpad=5)
+        ax.set_ylabel('Error (mm)', fontdict=font_bl, labelpad=5)
+        ax.set_xscale('log', nonposx="mask")
+        ax.set_yscale('log', nonposy="mask")
+
+        pfi_figure_time_vs_error = jph(pfo_output_A4_SE2, 'graph_time_vs_error.pdf')
+        plt.savefig(pfi_figure_time_vs_error, dpi=150)
+
+        plt.show(block=True)
 
 
 if __name__ == '__main__':
@@ -81,7 +221,7 @@ if __name__ == '__main__':
     control_ = {'svf_dataset'   : 'rotation',  # can be rotation, linear, homography, gauss, brainweb, adni
                 'computation'   : 'IC',  # can be IC, SA, SE
                 'collect'       : True,
-                'show'          : True}
+                'show_graphs'   : True}
 
     verbose_ = 1
     three_assessments_collector(control_, verbose_)
